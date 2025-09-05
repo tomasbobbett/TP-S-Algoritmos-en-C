a3689ff9b1f67ec7c1df2a0394a69a38e5410634
tomasbobbett <tbobbett@fi.uba.ar>
2025-09-05 19:56:02 -0300

    Leer archivo casi

--- src/tp1.c
+++ src/tp1.c
@@ -34,5 +34,24 @@ tp1_t *tp1_leer_archivo(const char *nombre) {
         return NULL; //error al reservar memoria de pokemones
     }
     char linea[256];
+    while(fgets(linea,sizeof(linea),file)){
+
+        int id, ataque, defensa, velocidad;
+        char nombre_poke[64];                       //Inicializo el pokemon de la linea actual
+        char tipo_str[16];      
+        int lineaActual = sscanf(linea,"%d,%63[^,],%15[^,],%d,%d,%d", &id,nombre_poke,tipo_str, &ataque,&defensa,&velocidad);
+
+        if (leidos != 6) {
+            continue; // línea inválida, la salto porque encontro mas o menos de 6 caracteristicas del pokemon
+        }
+
+        //verificar si ya existe el pokemon en tp
+
+        //verificar si modifico la capacidad de tp REDIM
+
+        //Añadir pokemon leido a &tp->pokemones[tp->cantidad]; y luego aumentar cantidad para no reemplazar el anterior
+        fclose(f)
+        return tp
+    }
 }
 
